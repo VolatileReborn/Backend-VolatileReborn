e2e331eeee62a30ad7bccfc8d6fc868c77717da1
LYK-love <74082944+LYK-love@users.noreply.github.com>
2022-10-01 17:26:50 +0800

    fix config

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -123,7 +123,7 @@ node("slave1") {
 
         stage("clean previous image and container"){
         sh "docker container rm -f ${EUREKA_CONTAINER_NAME}"
-        sh "docker container rm -f ${COLLECT_CONTAINER_PORT}"
+        sh "docker container rm -f ${COLLECT_CONTAINER_NAME}"
 
 //        sh "docker image rm ${EUREKA_IMAGE_TO_RUN}"
 //        sh "docker image rm ${COLLECT_IMAGE_TO_RUN}"
