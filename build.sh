c85b2fe97d2ca0e43756e06422fda011b36f2c53
LYK-love <74082944+LYK-love@users.noreply.github.com>
2022-10-01 16:01:25 +0800

    fix config

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 cd ./eureka-server
 mvn clean package
 
-cd ./collect-service
+pwd
+
+cd ../collect-service
+
+pwd
 #mvn clean package  -X org.jacoco:jacoco-maven-plugin:report  -Dmaven.test.failure.ignore=true
 mvn clean package
